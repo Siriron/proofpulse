559e654bfb2b67c364746cf6b781ed7b3d650ac6
sirironheart <127705750+Siriron@users.noreply.github.com>
2026-09-11 14:20:40 +0600

    Add files via upload

--- src/config/chains.js
+++ src/config/chains.js
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS='0x82E952a1a3F126b30de8E0C0beFeCc3012438360'
+export const CONTRACT_ADDRESS='0x431607C901544dd33631aA7aC2eA966b29326089'
 export const EXPLORER_URL='https://explorer-studio.genlayer.com'
 export const RPC_URL='https://studio.genlayer.com/api'
 export const STUDIONET_CONFIG={chainId:'0xF22F',chainName:'GenLayer StudioNet',rpcUrls:[RPC_URL],nativeCurrency:{name:'GEN',symbol:'GEN',decimals:18},blockExplorerUrls:[EXPLORER_URL]}
